0ee6ea66c65d298add4e17d1be86048c6f3656e3
CyraxVip <128605688+CyraxVip@users.noreply.github.com>
2023-11-01 19:39:26 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a21s-user
-add_lunch_combo omni_a21s-userdebug
-add_lunch_combo omni_a21s-eng
+add_lunch_combo twrp_a21s-user
+add_lunch_combo twrp_a21s-userdebug
+add_lunch_combo twrp_a21s-eng
