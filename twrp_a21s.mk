87cb8d7751a6bd5264dda29fed922ea2f101d396
CyraxVip <128605688+CyraxVip@users.noreply.github.com>
2023-11-01 19:33:35 +0700

    Update and rename omni_a21s.mk to twrp_a21s.mk

--- twrp_a21s.mk
+++ twrp_a21s.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a21s device
 $(call inherit-product, device/samsung/a21s/device.mk)
 
 PRODUCT_DEVICE := a21s
-PRODUCT_NAME := omni_a21s
+PRODUCT_NAME := twrp_a21s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A217F
 PRODUCT_MANUFACTURER := samsung
