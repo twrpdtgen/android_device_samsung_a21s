6dcf9e6f204751aea5ad2217f163fbb0c36e0140
CyraxVip <128605688+CyraxVip@users.noreply.github.com>
2023-11-04 08:29:06 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,12 +1,17 @@
-# Dynamic partitions
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#Dynamic Partition Flag
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+# Lpdump
+TW_EXCLUDE_LPDUMP := true
 
-# Screen
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1600
-DEVICE_RESOLUTION := 720x1600
+# Python prebuilts
+TW_EXCLUDE_PYTHON := true
+
+LOCAL_PATH := device/samsung/a21s
